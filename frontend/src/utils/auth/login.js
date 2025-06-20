847d84832db5e8ad36574006a2337a34db3fb727
ibraum <ibraumodnok@gmail.com>
2025-06-20 15:09:13 +0000

    feat: login page OK...

---
+++ frontend/src/utils/auth/login.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+import { defineStore } from "pinia";
+import { ref } from "vue";
+const userInfos = ref({})
+const token = ref('')
+
+export const useLoginStore = defineStore('login',  () => {
+    const login = async (credentials) => {
+        try {
+            const response = await axios.post(
+                `${import.meta.env.VITE_API_URL}auth/login`, 
+                {
+                    email: credentials.email,
+                    mot_de_passe: credentials.password,
+                }
+            );
+            console.log('try, ', response.data.user.email)
+            userInfos.value = response.data
+            if (response.data.user.email == credentials.email) {
+                console.log('router, ', response.data.user.email)
+                token.value = userInfos.token
+                localStorage.setItem('token', token);
+                return true
+            }
+    
+        } catch (error) {
+            console.log("HAHA "+error)
+            if (error.response) {
+                console.error('Statut HTTP :', error.response.status);
+                console.error('Message :', error.response.data);
+              } else if (error.request) {
+                console.error('Aucune réponse reçue :', error.request);
+              } else {
+                // console.error('Erreur');
+              } 
+        }
+
+        return false
+    }
+
+    return {login}
+});
